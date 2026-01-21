a763b7c09f488c80b5a49102a40e40dc27fb1538
Geazi Oliveira <geazioliveira@gmail.com>
2026-01-20 21:49:14 -0300

    Initial commit

---
+++ src/test/java/org/example/appleinterview/AppleInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.appleinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
